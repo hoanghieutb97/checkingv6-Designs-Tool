1366cc7983df652fd2a92da7a47465b8abdf3c18
hoanghieutb97 <hoanghieutb97@gmail.com>
2025-07-11 11:50:11 +0700

    vdfvbfs

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,9 @@ export const GLLM = "https://sheet.best/api/sheets/e8876c80-1778-414d-ae68-af6b9
 // PRODUCTS đã chuyển sang dùng MongoDB, không cần export nữa.
 
 // API Configuration
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = '192.168.1.240:5000';
 export const API_URL = `${API_BASE_URL}/api`;
 
 // Upload Configuration
-export const UPLOAD_BASE_URL = 'http://192.168.1.194:3008';
+export const UPLOAD_BASE_URL = 'http://192.168.1.240:3008';
 export const UPLOAD_URL = `${UPLOAD_BASE_URL}/upload`;
